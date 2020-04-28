c15d153e6b16576d4e826d6406470d4931e262b3
Pedro Mass <pedro.j.mass@gmail.com>
2020-04-28 11:26:01 -0400

    progress indicator: use null check
    Falsey was blocking when value was 0

--- index.user.js
+++ index.user.js
@@ -77,7 +77,7 @@ function getGoalStats() {
 }
 
 function getProgressIndicator(ideal, current) {
-  if (!current) return ''
+  if (current == null) return ''
 
   if (ideal <= current) {
     return '🎉'
